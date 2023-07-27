241c4029613529d607be9729267af093f463ccd5
shazmeen.shauque <shazmeen.shauque@ideofuzion.com>
2023-07-27 14:35:53 +0500

    initial commit, setup packages

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { WagmiConfig, configureChains, createConfig } from 'wagmi'
+import { goerli, mainnet } from 'wagmi/chains'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { publicProvider } from 'wagmi/providers/public'
 
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet, goerli],
+  [publicProvider()],
+)
+
+const config = createConfig({
+  autoConnect: true,
+  publicClient,
+  webSocketPublicClient,
+  connectors: [
+    new MetaMaskConnector({
+      chains,
+      options: {
+        UNSTABLE_shimOnConnectSelectAccount: true,
+      },
+    }),
+  ]
+})
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig config={config}>
+      <App />
+    </WagmiConfig>
+
   </React.StrictMode>
 );
 
